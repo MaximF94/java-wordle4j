8001b1f88321dbd0426266398e2843220a9b6bdb
Maxim Filippov <mr.maks.filippov.94@mail.ru>
2026-02-11 17:08:50 +0300

    Исправил ошибки при проверке ветки проекта

--- src/ru/yandex/practicum/TooManyAttemptsException.java
+++ src/ru/yandex/practicum/TooManyAttemptsException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-public class TooManyAttemptsException extends Throwable{
+public class TooManyAttemptsException extends Throwable {
 
     public TooManyAttemptsException(String message) {
         super(message);
